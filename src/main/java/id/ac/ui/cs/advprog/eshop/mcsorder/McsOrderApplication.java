76c05b770760f68199e1e561b270b0653df62b32
bryanjeshua <bryanjmt04@gmail.com>
2024-04-06 15:55:03 +0700

    initialization

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsorder/McsOrderApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcsorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(McsOrderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/mcsorder/McsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.eshop.mcsorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
