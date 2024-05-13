70a395a6e029eef5882f7a5210bfd669c6239820
Ahmad Rafi Wirana <ahmad.rafi02@ui.ac.id>
2024-05-13 18:42:03 +0700

    feat: adding order implementation, order test, and initializing payment service

--- src/main/java/id/ac/ui/cs/advprog/eshop/mcsorder/model/OrderItem.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/mcsorder/model/OrderItem.java
@@ -1,13 +1,29 @@
+// OrderItem.java
 package id.ac.ui.cs.advprog.eshop.mcsorder.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter; 
 import lombok.Setter;
 
-
-@Getter @Setter
+@Entity
+@Getter
+@Setter
 public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    
+    private String productName;
+    private int quantity;
+    private double price;
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
     private Order order;
-    private Long productId;
-    private Integer quantity;
-    private Double price;
+    
 }
